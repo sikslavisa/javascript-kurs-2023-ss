3bd01ca1ed95c2beb3c553b461cad7da07b79a22
sikslavisa <133794415+sikslavisa@users.noreply.github.com>
2023-05-16 18:55:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello from git repository"); 
+
+//git clone #link reposotorijuma -- klonira repositorijum
+//git status -- provjerava da li ima promjena na lokalnoj mašini
+//git add "ime fajla" - A ili ako se želi dodati više fajlova nakon git add -ALL
+//git commit script.js -m"Added script.js file with git comments" a zatim se izvrši komanda git push
+
+console.log("TEST");
